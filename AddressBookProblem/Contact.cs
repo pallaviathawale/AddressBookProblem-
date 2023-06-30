0ead67a2e94dd383872185a043c5f3b999962855
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 22:51:08 +0530

    [PALLAVI] Add: CreateContact

---
+++ AddressBookProblem/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    public class Contact
+    {
+        private Dictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+        public static AddressBook AddContact()
+        {
+            AddressBook contact = new AddressBook();
+            Console.WriteLine("Enter First name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code");
+            contact.ZipCode = (int)Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            contact.PhoneNumber = (int)Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address");
+            contact.Email = Console.ReadLine();
+            return contact;
+
+        }
+        
+        
+    }
+}
+
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to AddressBook Program");
+            Console.WriteLine("Welcome to AddressBook Program");
+            
+                    AddressBook addressBook = new AddressBook();
+                    addressBook = Contact.AddContact();
+            
         }
     }
 }
