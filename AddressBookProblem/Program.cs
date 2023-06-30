5139a4af0d29d7669f662b42d951342508e8ff2b
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 23:16:43 +0530

    [PALLAVI] Add: Add Contact In AddressBook

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -5,10 +5,33 @@
         static void Main(string[] args)
         {
             Console.WriteLine("...Welcome to Address Book...");
-            AddressBook addressBook = new AddressBook();
-            addressBook.CreateContact();
+           AddressBook addressBook = new AddressBook();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBook.CreateContact();
+                        break;
+                    case 2:
+                        addressBook.AddContact();
+                        break;
+                    case 3:
+                        addressBook.DisplayContact();
+                        break;
+                    case 4 :
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct number ! ");
+                        break;
+                }
+            }
             Console.ReadLine();
-
         }
     }
+    
 }
